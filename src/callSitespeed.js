7127f7f1faed1500b7822849a0203b24fcea04cd
rmparr <ryan@ryanmparr.com>
2020-03-11 14:45:37 +0100

    Add functon to send a call to sitespeed and return results.
    This function is writing to the tmp directory, which will have to be rsynced to a remote directory.

---
+++ src/callSitespeed.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const sitespeed = require('sitespeed.io')
+
+const callSitespeed = async function(url) {
+  const urls = [url];
+  const name = new URL(urls[0]).host;
+  const timestamp = Date.now();
+
+  return await (async function run(){
+    try {
+      const result = await sitespeed.run({
+        urls,
+        browsertime: {
+          iterations: 1,
+          connectivity: {
+            profile: 'native',
+            downstreamKbps: undefined,
+            upstreamKbps: undefined,
+            latency: undefined,
+            engine: 'external',
+          },
+          headless: true,
+          browser: 'firefox'
+        },
+        sustainable: {
+          enable: true,
+          pageViews: 100000,
+          useGreenWebHostingAPI: true
+        },
+        firstParty: true,
+        outputFolder: `tmp/${name}-${timestamp}`,
+        name: name
+      });
+      return result;
+    } catch (e) {
+      return e;
+    }
+  })();
+};
+
+module.exports = callSitespeed;
