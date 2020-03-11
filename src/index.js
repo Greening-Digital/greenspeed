a615b616afe24124dd0b6991ab1e06878b6170e9
rmparr <ryan@ryanmparr.com>
2020-03-11 14:53:14 +0100

    Update index.js
    The handler now sends an async call to the callSitespeed function, returning either a list of errors or a success message.
    
    The handler now also validates a correctly formed URL is passed in.
    
    The entire server is exported so it can be tested.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,63 @@
+'use strict';
+
 const Hapi = require('@hapi/hapi');
 
-const init = async () => {
+const callSitespeed = require('./callSitespeed');
 
-  const server = Hapi.server({
-    port: process.env.PORT || 3000,
-    host: 'localhost'
-  });
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: 'localhost'
+});
 
-  server.route({
-    method: 'POST',
-    path: '/',
-    handler: (request, h) => {
-      const payload = request.payload
+server.route({
+  method: 'POST',
+  path: '/',
+  handler: async (request, h) => {
+    const url = request.payload.url
 
-
-      if (!payload) {
-        const msg = 'no Pub/Sub message received';
-        console.error(`error: ${msg}`);
-        return h.response(`Bad Request: ${msg}`).code(400)
-      }
-
-      const pubSubMessage = payload.message;
-
-      const name = pubSubMessage.data
-        ? Buffer.from(pubSubMessage.data, 'base64')
-          .toString()
-          .trim()
-        : 'World';
-
-      console.log(`Hello ${name}!`);
-
-      return h.response('created').code(204)
-    },
-    options: {
-      auth: false,
-      // validate: {
-      // payload: {
-      //     url: TODO
-      // Insert your joi schema here
-      //  https://hapi.dev/family/joi/tester/
-      // Joi.string().uri({
-      //   scheme: [
-      //     'http',
-      //     'https'
-      //    ]
-      // })
-
-      // }
-      // }
+    if (!url) {
+      const msg = 'no URL received';
+      return h.response(`Bad Request: ${msg}`).code(400);
     }
 
-  });
+    if (isUrl(url)) {
+      const result = await callSitespeed(url);
+      if (result.errors && result.errors.length > 0) {
+        return h.response(result.errors).code(400);
+      } else {
+        return h.response('created').code(204);
+      }
+    } else {
+      const msg = 'bad URL received';
+      return h.response(`Bad Request: ${msg}`).code(400);
+    }
+  },
+  options: {
+    auth: false,
+    // validate: {
+    // payload: {
+    //     url: TODO
+    // Insert your joi schema here
+    //  https://hapi.dev/family/joi/tester/
+    // Joi.string().uri({
+    //   scheme: [
+    //     'http',
+    //     'https'
+    //    ]
+    // })
 
+    // }
+    // }
+  }
+
+});
+
+const isUrl = function(url) {
+  const regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+  return regexp.test(url);
+}
+
+const init = async () => {
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
@@ -64,3 +70,5 @@ process.on('unhandledRejection', (err) => {
 });
 
 init();
+
+module.exports = server;
