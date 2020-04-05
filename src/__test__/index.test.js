7cfd33248e007a421348cc331ace84168040c187
Chris Adams <chris@productscience.co.uk>
2020-04-05 18:46:27 +0200

    Rename and rearrange files

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { init, start } = require('../webui');
+const { init, start } = require('../index');
+
 
 jest.mock('../callSitespeed');
 
@@ -67,32 +68,3 @@ describe("API Server", () => {
   });
 
 })
-describe("WebUI Server", () => {
-
-  let server;
-
-  beforeEach(async () => {
-    server = await init();
-  });
-
-  afterEach(async () => {
-    await server.stop();
-  });
-
-
-  test.only("GET / - serves a static page for submissions", async () => {
-    const options = {
-      method: 'GET',
-      url: '/'
-    };
-    const data = await server.inject(options);
-    expect(data.statusCode).toBe(200);
-  })
-
-  test.todo("Submitting a site, takes you to a waiting page for the results")
-
-  test.todo("Submitting a site, saves the site, and pending of the run")
-
-  test.todo("Waiting page for the results are updated after its greenspeed run has completed")
-})
-
